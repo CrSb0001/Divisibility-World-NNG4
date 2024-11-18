c4d9caacd07da6c840bf8552806c5e8a52a2ae83
CrSb0001 <127436709+CrSb0001@users.noreply.github.com>
2024-11-18 14:45:38 -0600

    Update DivisibilityWorld.lean

--- Game/Levels/DivisibilityWorld.lean
+++ Game/Levels/DivisibilityWorld.lean
@@ -1,1 +1,14 @@
+/-- import Game.Levels.DivisibilityWorld.L01_dvd_add -/
 
+World "DivisibilityWorld"
+Title "Divisibility World"
+
+Introduction "
+Advanced Multiplication World proved various implications involving multiplication, such as
+the `mul_le_mul_right` theorem `a ≤ b → a * t ≤ b * t`.
+Also, you also worked with proving `eq_succ_of_ne_zero`, that is `a ≠ 0 → ∃n, a = succ n`.
+"
+
+Info "
+Here you'll be proving theorems about divisibility of numbers and prime numbers.
+"
