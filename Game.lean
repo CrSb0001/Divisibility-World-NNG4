2802ccadaa54a4a80efad3b9612a27192b16b088
CrSb0001 <127436709+CrSb0001@users.noreply.github.com>
2024-11-18 19:15:57 -0600

    Update Game.lean

--- Game.lean
+++ Game.lean
@@ -1,8 +1,9 @@
 import GameServer.Commands
-import Game.Levels.DivisibilityWorld
+/-- import Game.Levels.DivisibilityWorld
 
 -- Here's what we'll put on the title screen
-Title "Divisibility World"
+/-- Title "Divisibility World" -/
+/--
 Introduction
 "
 Fanmade addition to NNG4 for divisibility world since it doesn't exist as of Nov 2024
@@ -16,13 +17,13 @@ Original game: adam.math.hhu.de
 
 Dependency Algorithm → DivisibilityWorld
 Dependency AdvMultiplication → DivisibilityWorld
-
+-/
 /-! Information to be displayed on the servers landing page. -/
-Languages "English"
-CaptionShort "Game Template"
-CaptionLong "You should use this game as a template for your own game and add your own levels."
--- Prerequisites "" -- add this if your game depends on other games
--- CoverImage "images/cover.png"
+/-- Languages "English" -/
+/-- CaptionShort "Game Template" -/
+/-- CaptionLong "You should use this game as a template for your own game and add your own levels." -/
+/-- -- Prerequisites "" -- add this if your game depends on other games -/
+/- -- CoverImage "images/cover.png" -/
 
 /-! Build the game. Show's warnings if it found a problem with your game. -/
-MakeGame
+/-- MakeGame -/
